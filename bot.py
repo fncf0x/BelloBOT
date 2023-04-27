6f36ee4c7aea5fa766de22a1216a0401fd221e58
fncf0x <aymenekhennache@gmail.com>
2023-04-27 11:07:13 +0200

    removed debug print, and added more msg

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ import discord
 from dotenv import load_dotenv
 from random import choice
 
-
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
@@ -25,16 +24,33 @@ question_answers = [
         ["baghyin tchofo zebi ?", "ahachmo malkom", "malkom a zebi"]
     ),
     (
-        ["weri nchofek", "baghi nchofek", "eb3at tswira"],
+        ["weri nchofek", "baghi nchofek", "eb3at tswira", 'photo', 'chof'],
         [("ha werani", "pictures/1.jpg"), ("ih omri", "pictures/2.jpg") ,("pa de bghoblem", "pictures/3.jpg") ,("ouiii ezin", "pictures/4.jpg") ,("lik ki rani dayer", "pictures/5.jpg") ,("ok bb", "pictures/6.jpg")]
+    ),
+    (
+        ["attack", "ezdem"],
+        ["nik mok", "kowdo", "waaaa3"]
+    ),
+    (
+        ['rak hna', 'jit', "mazalaek hna", 'u here', "here"],
+        ["wah ni hna", "cha khasek omri", "ouiiii biensour", "rana daymen hna", "wah wah hna jamais la nroho"]
+    ),
+    (
+        ["love you", "na3che9 fik", "nhabek", "omri"],
+        ["moi aussi nebghik", "nbhghik rebek", "nhabek cheuri", "malek?", "aya privé nahki m3ak", 'kwdo rani za3fan'],
+    ),
+    (
+        ["chbeb", "li zomme", "bogoss", "tawe3na", "te3na"],
+        ["merciiiii hafdek", "ooooh li zomme", "hafdk hafdek", "sa77aa 3ayniya", "mchi we9tha manich mlih"],
     )
 ]
 
 def answer(msg):
     for questions, answers in question_answers:
         if any(question in msg.content.lower() for question in questions) and "bello" in msg.content.lower():
-            print(answers)
             return choice(answers)
+    if "bello" in msg.content.lower():
+        return choice(["not implemented ya kho dir PR", "mafhatmch mazalni net3alem", "khatini mana3refch", "?"])
 
 class MyClient(discord.Client):
     async def on_ready(self):
